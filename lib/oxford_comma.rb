4e57acac1aad98fde2695b65549d2021caf00a03
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-05-16 22:15:08 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    case array.size
+    when 0
+      ""
+    when 1
+      array[0]
+    when 2
+      "#{array[0]} and #{array[1]}"
+    else
+      last_element = array.pop
+      string = array.join(", ")
+      string << ", and #{last_element}"
+    end
 end
